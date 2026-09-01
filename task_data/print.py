b983c9115cbfc5a70dbeb37005a585c7f9778fcd
Mina <minagrio01@gmail.com>
2026-08-31 20:39:29 -0500

    añadi una lista de comidas :D

--- task_data/print.py
+++ task_data/print.py
@@ -1,5 +1,7 @@
 print('Buenas tardes')
 def despedir(nombre):
     print(f'Buenas noches, {nombre}')
-
+comidas = ['Arroz Chaufa', 'Mariscos', 'Ceviche', 'Carapulcra']
+    
+    
 #Comentario chevere
